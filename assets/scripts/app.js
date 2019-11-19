20ac7f862799f41f86d89a6d4e9d9eb266b10a18
Nacer Bouchekhima <nbouchek@gmail.com>
2019-11-19 16:03:20 +0000

    Select add-model element

---
+++ assets/scripts/app.js
@@ -0,0 +1,7 @@
+const addMovieModal = document.getElementById("add-modal");
+
+// The following also works but might be slower
+// const addMovieModal2 = document.querySelector("#add-modal");
+// const addMovieModal3 = document.body.children[1];
+
+
