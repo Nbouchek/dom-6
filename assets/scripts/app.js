425bdea713b245dabc666f8b7588a1ff2481e42f
Nacer Bouchekhima <nbouchek@gmail.com>
2019-11-19 16:03:54 +0000

    Select button in header

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,4 +4,7 @@ const addMovieModal = document.getElementById("add-modal");
 // const addMovieModal2 = document.querySelector("#add-modal");
 // const addMovieModal3 = document.body.children[1];
 
+const startAddMovieButton = document.querySelector("header button");
+// const startAddMovieButton = document.querySelector("header").lastElementChild;
+
 
