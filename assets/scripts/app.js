4e554648b747d4f07962689865d4145b3ebfec6b
Nacer Bouchekhima <nbouchek@gmail.com>
2019-11-19 18:57:02 +0000

    Store movies locally.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -10,12 +10,11 @@ const startAddMovieButton = document.querySelector("header button");
 const backdrop = document.getElementById("backdrop");
 // const backdrop = document.body.firstElementChild;
 
-const cancelAddMovieButton = addMovieModal.querySelector(
-  ".btn--passive"
-);
-
+const cancelAddMovieButton = addMovieModal.querySelector(".btn--passive");
 const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling;
 const userInputs = addMovieModal.querySelectorAll("input");
+
+const movies = [];
 
 const toggleBackdrop = () => {
   backdrop.classList.toggle("visible");
@@ -49,6 +48,16 @@ const addMovieHandler = () => {
     alert("Please enter valid values (rating between 1 and 5).");
     return;
   }
+
+  const newMovie = {
+    title: titleValue,
+    image: imageUrlValue,
+    rating: ratingValue
+  };
+
+  movies.push(newMovie);
+  console.log(movies);
+  toggleMovieModel();
 };
 
 startAddMovieButton.addEventListener("click", toggleMovieModel);
